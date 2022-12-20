1aee1174e5f81f89682e7762e52c699a115518c1
MSiyf212 <121081559+MSiyf212@users.noreply.github.com>
2022-12-20 20:29:29 +0700

    Update BST_DataFrame.py

--- BST_DataFrame.py
+++ BST_DataFrame.py
@@ -1,3 +1,49 @@
+import pandas
+import numpy 
+data = pandas.read_csv("https://raw.githubusercontent.com/Mif212/Struktur_Data/main/Cleaned_Laptop_data.csv")
+data.drop(["processor_brand", "processor_gnrtn", "ram_type", "os", "os_bit", "graphic_card_gb", "weight", "display_size", "warranty", "Touchscreen", "msoffice", "old_price", "discount", "star_rating", "ratings", "reviews"], axis=1, inplace=True)
+data = data.drop_duplicates()
+data
+
+# Searching data
+class Search:
+    def __init__(self, data):
+        self.data = data
+        self.col = []
+        for i in self.data:
+            self.col.append(i)
+    def searching(self, column, item):
+        if column in self.col:
+            if item not in list(self.data[column]):
+                return f"No {item} in Columns {column}"
+            else:
+                return self.data[self.data[column] == item]
+        else:
+            return f"No Columns {column} in Data Frame" 
+
+# Mencari nilai yang paling dekat dengan mean
+def closest_average(data):
+    dt = numpy.array(data)
+    mean = dt.mean()
+    selisih = abs(dt[0] - mean)
+    for i in dt:
+        if abs(i - mean) <= selisih:
+            selisih = abs(i - mean)
+            root = i
+    return root
+
+# DataFrame ke list
+def df_to_list(data):
+    for i in data:
+        kol = i
+    list_data = []
+    for j in range(len(list(data[kol]))):
+        dic = {}
+        for k in data:
+            dic[k] = list(data[k])[j]
+        list_data.append(dic)
+    return list_data
+
 class BST:
   def __init__(self, key):
     self.key = key
@@ -104,3 +150,35 @@ class BST:
       elif root.key["latest_price"] < key["latest_price"]:
         search = self.searchNode(root.right, key)
     return search
+
+  
+  cari_root = Search(data)
+ketemu = cari_root.searching("latest_price", closest_average(data["latest_price"]))
+key = {}
+for i in ketemu:
+    key[i] = list(ketemu[i])[0]
+list_data = df_to_list(data)
+
+root = BST(key)
+for j in list_data:
+    root.insert(root, j)
+root.inorder(root)
+
+#tambah data 
+amsiluman = {"brand" : "Amsiluman", "model" : "AXE", "processor_name" : "Core i20", "ram_gb" : "100 GB", "ssd" : "100 TB", "hdd" : "0 GB", "latest_price" : 300000}
+root.insert(root, amsiluman)
+laptop_2000_tahun_mendatang = {"brand" : "212XY", "model" : "ALIENFIRE", "processor_name" : "Hard Core", "ram_gb" : "100 TB", "ssd" : "10000 TB", "hdd" : "0 GB", "latest_price" : 308000}
+root.insert(root, laptop_2000_tahun_mendatang)
+root.inorder(root)
+
+#search data
+asus = {"brand":"ASUS", "model":"Zephyrus", "processor_name":"Core i9", "ram_gb":"16 GB GB", "ssd":"3072 GB", "hdd":"0 GB", "latest_price":441990}
+print(root.searchNode(root, asus)) # True
+print(root.searchNode(root, amsiluman)) # True
+#delete asus
+root.deleteNode(root, asus)
+print(root.searchNode(root, asus)) # False
+
+print(root.key) # cek root/akar pohon
+print(root.right.left.right.key) # cek -> kanan -> kiri -> kanan (pada pohon)
+print(root.left.left.right.right.key) # cek -> kiri -> kiri -> kanan -> kanan (pada pohon)
